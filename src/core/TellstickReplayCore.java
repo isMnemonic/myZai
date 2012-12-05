1c11e2d9328bdfa52700d979fc01e4d883cacd2b
Per Fransman <per@franzman.se>
2012-12-05 16:27:11 +0100

    added more features from telldus c api

--- src/core/TellstickReplayCore.java
+++ src/core/TellstickReplayCore.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import it.sauronsoftware.cron4j.*;
-import com.sun.jna.*;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.ptr.*;
 
 public class TellstickReplayCore {
+	
 	public interface CLibrary extends Library{
 		void tdInit();
 		void tdClose();
@@ -15,20 +15,47 @@ public class TellstickReplayCore {
 		int tdSensorValue(byte[] protocol, byte[] model, int id, int dataType, byte[] value, int valueLength, IntByReference timestamp);
 		int tdGetNumberOfDevices();
 		int tdGetDeviceId( int deviceId );
+		char tdGetName( int deviceId );
+		void tdReleaseString( char string );
+	}
+	
+	public enum Methods{
+		TELLSTICK_TURNON, TELLSTICK_TURNOFF, TELLSTICK_BELL
 	}
 	
 	public static void main(String[] args) {
 		CLibrary libTelldus = (CLibrary)Native.loadLibrary("libtelldus-core.so.2", CLibrary.class);
 		
-		libTelldus.tdInit(); //Initiate the telldus core.
+		libTelldus.tdInit(); 									//Initiate the telldus api.
 		
-		//Test: list devices.
-		int nrOfDevices = libTelldus.tdGetNumberOfDevices();
-		for ( int i = 0; i < nrOfDevices; i++ ) {
-			int id = libTelldus.
+		int nrOfDevices = libTelldus.tdGetNumberOfDevices();	//Test: list devices.
+		for ( int i = 0; i < nrOfDevices; i++ ) {				//Initiate loop.
+			int id = libTelldus.tdGetDeviceId( i );				//Retrives the unique id on current index.
+			char name = libTelldus.tdGetName( id );				//Retrives the name of the device by unique id.
+			System.out.println( name );							//Simple printline. To be removed.
+			libTelldus.tdReleaseString( name );					//Release the resource in 'char name'
 		}
 		
-		libTelldus.tdClose(); //Close the telldus core. Clean up is done inside libtelldus-core.so.2
+		libTelldus.tdClose(); 									//Close the telldus api. Clean up is done inside 'libtelldus-core.so.2'
 		
+	}
+	
+	private void checkDeviceFeatures( int id ){
+		
+	}
+	
+	private boolean DimDeviceById( int id ) {
+		
+		return false;
+	}
+	
+	private boolean DimDeviceByGroup( String name ) {
+		
+		return false;
+	}
+	
+	private boolean TurnDeviceOffById( int id ){
+		
+		return false;
 	}
 }
