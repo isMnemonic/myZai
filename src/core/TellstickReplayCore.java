13963c93745c3e938b15fb96e06f65212cab3a2c
Per Fransman <per@franzman.se>
2012-12-07 17:17:21 +0100

    remapped function types

--- src/core/TellstickReplayCore.java
+++ src/core/TellstickReplayCore.java
@@ -16,50 +16,50 @@ public class TellstickReplayCore {
 		int tdSensor( char protocol, int protocolLen, char model, int modelLen, int id, int dataTypes );
 		int tdSensorValue( char protocol, char model, int id, int dataType, char value, int len, int timestamp );
 		int tdController( int controllerId, int controllerType, char name, int nameLen, int available );
-		int tdControllerValue( int controllerId, char name, char value, int valueLen );
-		int tdSetControllerValue( int controllerId, char name, char value );
+		int tdControllerValue( int controllerId, String name, String value, int valueLen );
+		int tdSetControllerValue( int controllerId, String name, String value );
 		int tdRemoveController( int controllerId );
 		int tdGetNumberOfDevices();
 		int tdGetDeviceId( int deviceId );
 		int tdGetDeviceType( int deviceId );
-		char tdGetName( int deviceId );
-		boolean tdSetName( int deviceId, char name );
-		char tdGetProtocol( int deviceId );
-		boolean tdSetProtocol( int deviceId, char protocol );
+		String tdGetName( int deviceId );
+		boolean tdSetName( int deviceId, String name );
+		String tdGetProtocol( int deviceId );
+		boolean tdSetProtocol( int deviceId, String protocol );
 		char tdGetModel( int deviceId );
-		boolean tdSetModel( int deviceId, char model );
-		boolean tdSetDeviceParameter( int deviceId, char name, char value );
-		char tdGetDeviceParameter( int deviceId, char name, char defaultValue );
+		boolean tdSetModel( int deviceId, String model );
+		boolean tdSetDeviceParameter( int deviceId, String name, String value );
+		String tdGetDeviceParameter( int deviceId, String name, String defaultValue );
 		int tdAddDevice();
 		boolean tdRemoveDevice( int deviceId );
 		int tdMethods( int id, int methodsSupported );
-		char tdGetErrorString( int errorNumber );
-		int tdSendRawCommand( char command, int reserved );
-		void tdSendRawCommand( int vid, int pid, char serial );
-		void tdConnectTellstickController( int vid, int pid, char serial );
-		void tdDisconnectTellstickController( int vid, int pid, char serial );				
-		void tdReleaseString( char string );
+		String tdGetErrorString( int errorNumber );
+		int tdSendRawCommand( String command, int reserved );
+		void tdSendRawCommand( int vid, int pid, String serial );
+		void tdConnectTellstickController( int vid, int pid, String serial );
+		void tdDisconnectTellstickController( int vid, int pid, String serial );				
+		void tdReleaseString( String value );
 		int tdBell( int deviceId );
-		int tdDim( int deviceId, char level );
+		int tdDim( int deviceId, String level );
 		int tdExecute( int deviceId );
 		int tdUp( int deviceId );
 		int tdDown( int deviceId );
 		int tdStop( int deviceId );
 		int tdLearn( int deviceId );
 		int tdLastSentCommand( int deviceId, int methodSupported );
-		char tdLastSentValue( int deviceId );
+		String tdLastSentValue( int deviceId );
 		
 	}
 	
 	public class TellstickDevice{
 		private final int id;
-		private final char name;
-		private final char model;
-		private final char protocol;
+		private final String name;
+		private final String model;
+		private final String protocol;
 		
 		private final Methods[] supported;
 		
-		public TellstickDevice( int _id, char _name, char _model, char _protocol, Methods[] _supported ) {
+		public TellstickDevice( int _id, String _name, String _model, String _protocol, Methods[] _supported ) {
 			this.id = _id;
 			this.name = _name;
 			this.model = _model;
@@ -71,15 +71,15 @@ public class TellstickReplayCore {
 			return id;
 		}
 
-		public char getName() {
+		public String getName() {
 			return name;
 		}
 		
-		public char getMode() {
+		public String getMode() {
 			return model;
 		}
 		
-		public char getProtocol() {
+		public String getProtocol() {
 			return protocol;
 		}
 
@@ -93,19 +93,20 @@ public class TellstickReplayCore {
 	}
 	
 	public static void main(String[] args) {
-		CLibrary libTelldus = (CLibrary)Native.loadLibrary("libtelldus-core.so.2", CLibrary.class);
+
+		CLibrary libTelldus = (CLibrary)Native.loadLibrary("TelldusCore", CLibrary.class);
 		
 		libTelldus.tdInit(); 									//Initiate the telldus api.
 		
 		int nrOfDevices = libTelldus.tdGetNumberOfDevices();	//Test: list devices.
 		for ( int i = 0; i < nrOfDevices; i++ ) {				//Initiate loop.
 			int id = libTelldus.tdGetDeviceId( i );				//Retrives the unique id on current index.
-			char name = libTelldus.tdGetName( id );				//Retrives the name of the device by unique id.
+			String name = libTelldus.tdGetName( id );			//Retrives the name of the device by unique id.
 			System.out.println( name );							//Simple printline. To be removed.
 			libTelldus.tdReleaseString( name );					//Release the resource in 'char name'
 		}
 		
-		libTelldus.tdClose(); 									//Close the telldus api. Clean up is done inside 'libtelldus-core.so.2'
+		libTelldus.tdClose(); 		
 		
 	}
 	
