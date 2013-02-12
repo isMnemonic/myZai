3b7688803dad2913a23413df9d05ef447e7db0b6
Per Fransman <per@franzman.se>
2013-02-12 22:48:05 +0100

    minor database query update

--- src/database/Compiler/TellstickReplay/DatabaseConnector/TellstickReplaySqlQuerys.java
+++ src/database/Compiler/TellstickReplay/DatabaseConnector/TellstickReplaySqlQuerys.java
@@ -18,5 +18,8 @@ public interface TellstickReplaySqlQuerys {
 	public static final String sqlSelectAllGroups = "SELECT id, name, description FROM cyxl_group;";
 	public static final String sqlSelectAllSchedules ="SELECT id, task, action_id, object_id FROM cyxl_schedule;";
 	
-	public static final String sqlSelectDeviceWithSchedule = "";
+	/**
+	 * SQL Query: Selects all devices with its corresponding schedules.
+	 */
+	public static final String sqlSelectDeviceWithSchedule = "SELECT d.id, d.title, d.protocol, d.model, d.description, d.supported, s.task, s.value, a.action FROM cyxl_device AS d, cyxl_schedule AS s, cyxl_action AS a WHERE d.id = s.object_id AND s.action_id = a.id ORDER BY d.id;";
 }
